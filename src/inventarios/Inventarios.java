56b3a926c83c724881e405b3399fcaf655ac59ad
Melvin <orlandotzunux@gmail.com>
2024-10-04 00:49:38 -0600

    Initial commit

---
+++ src/inventarios/Inventarios.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventarios;
+
+/**
+ *
+ * @author Farasi
+ */
+public class Inventarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Usar una expresión lambda para mostrar el frmLogin
+        frmLogin login = new frmLogin();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+    
+}
